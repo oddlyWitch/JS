79d57c48d9851536aa9719bcd431b31397fd2b47
Swetha Ch <swethabliss98@gmail.com>
2025-02-03 19:07:09 +0530

    new changes

--- test.js
+++ test.js
@@ -1,2 +1,18 @@
 // the hello world program
 alert("Hello, World!");
+
+
+const http = require('http');
+
+const username = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req,res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello world xxxx');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
